7bb66731f595d475440af6d735e23d1d5be19789
Jeffrin James <jeffrinjames99@gmail.com>
2025-05-15 17:30:01 +0530

    Delete AdSenseScript.js

--- AdSenseScript.js
+++
@@ -1,6 +0,0 @@
-// AdSenseScript.js
-const script = document.createElement('script');
-script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8061354259054600 ';
-script.async = true;
-script.crossOrigin = 'anonymous';
-document.head.appendChild(script);
