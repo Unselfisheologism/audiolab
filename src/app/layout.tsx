57552919d908a01ecbb8d3a3ce915e38f536285b
Jeffrin James <jeffrinjames99@gmail.com>
2025-05-17 15:46:38 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,18 +17,21 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body className={`font-sans bg-background text-foreground`}> {/* Added bg-background and text-foreground for base styling */}
-      <div class="sf-root" data-id="3877164" data-badge="oss-users-love-us-white" style="width:125px">
+      <div className={{sf-root}} data-id={{3877164}} data-badge={{oss-users-love-us-white}} style={{width:125px}}>
           <a href="https://sourceforge.net/projects/audiolab/" target="_blank">Audiolab</a>
       </div>
-<script>(function () {var sc=document.createElement('script');sc.async=true;sc.src='https://b.sf-syn.com/badge_js?sf_id=3877164';var p=document.getElementsByTagName('script')[0];p.parentNode.insertBefore(sc, p);})();
-</script>
-<!-- End  Tag -->
+      <script>(function () {var sc=document.createElement('script');sc.async=true;sc.src='https://b.sf-syn.com/badge_js?sf_id=3877164';var p=document.getElementsByTagName('script')[0];p.parentNode.insertBefore(sc, p);})();
+      </script>
         <a href="https://fazier.com" target="_blank"><img src="https://fazier.com/api/v1//public/badges/launch_badges.svg?badge_type=featured&theme=neutral" alt="Fazier badge" /></a>
         <a href="https://www.uneed.best/tool/audiolab">
           <img src="https://www.uneed.best/EMBED3.png" alt="Uneed Embed Badge" />
         </a>
-        <a href='https://www.sideprojectors.com/project/58769/audiolab' alt='Check out Audiolab  at @SideProjectors'><img src='https://www.sideprojectors.com/img/badges/badge_show_black.png' alt='Check out Audiolab at @SideProjectors'></a>
-        <a href="https://dang.ai/" target="_blank" ><img src="https://cdn.prod.website-files.com/63d8afd87da01fb58ea3fbcb/6487e2868c6c8f93b4828827_dang-badge.png" alt="Dang.ai" style="width: 150px; height: 54px;" width="150" height="54"/></a>  
+        <a href='https://www.sideprojectors.com/project/58769/audiolab' alt='Check out Audiolab  at @SideProjectors'>
+          <img src='https://www.sideprojectors.com/img/badges/badge_show_black.png' alt='Check out Audiolab at @SideProjectors' />
+        </a>
+        <a href='https://dang.ai/' target='_blank' >
+          <img src='https://cdn.prod.website-files.com/63d8afd87da01fb58ea3fbcb/6487e2868c6c8f93b4828827_dang-badge.png' alt='Dang.ai' style='width: 150px; height: 54px;' width='150' height='54' />
+        </a>  
         {children}
         <Toaster />
       </body>
