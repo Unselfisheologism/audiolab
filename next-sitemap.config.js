a3abc558c65cc3ab18ae82c541671b65f0405e22
drtajames <drtajames@gmail.com>
2025-05-23 16:45:09 +0000

    site golbal

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 export default {
     siteUrl: 'https://audiolab.in.net', // Replace with your site's URL
-    generateRobotsTxt: false, // Disable robots.txt file generation
+    generateRobotsTxt: true, // Enable robots.txt file generation
+    additionalPaths: async (config) => [
+        { loc: '/audio-forge', changefreq: 'weekly', priority: 0.7 },
+        { loc: '/faq', changefreq: 'monthly', priority: 0.5 },
+        { loc: '/terms', changefreq: 'monthly', priority: 0.5 },
+        { loc: '/privacy', changefreq: 'monthly', priority: 0.5 },
+        { loc: '/cookies', changefreq: 'monthly', priority: 0.5 },
+        { loc: '/licenses', changefreq: 'monthly', priority: 0.5 },
+    ],
     outDir: './out', // Ensure it matches your 'output' directory in next.config.ts
 };
