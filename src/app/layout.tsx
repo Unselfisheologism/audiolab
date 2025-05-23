a1b5bf2e95b1da2dc241143f8b473c3856791840
Unselfisheologism <jeffrinjames99@gmail.com>
2025-05-23 13:11:29 +0530

    side cloud

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -425,7 +425,7 @@ export default function RootLayout({
           </a>
         </div>  
         <div className="badge-wrapper-130x40">
-          <a href='https://www.sideprojectors.com/project/58769/audiolab' target="_blank" rel="noopener noreferrer">
+          <a href='https://res.cloudinary.com/ddz3nsnq1/image/upload/v1747986022/side_cloud_y0rtcq.png' target="_blank" rel="noopener noreferrer">
             <Image
               src='https://www.sideprojectors.com/img/badges/badge_show_black.png'
               alt='Check out Audiolab at @SideProjectors'
